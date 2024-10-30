c9a88ff61a1312c293a442fe44cbcca9bb284c60
your-correct-username <takhminaacom@gmail.com>
2024-10-30 14:31:30 +0600

    com

--- one/payment/urls.py
+++ one/payment/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreatePaymentView, ExecutePaymentView, CancelPaymentView
+from .views import PaymentCreateView, PayPalExecuteView, PaymentHistoryView, PayPalCancelView
 
 urlpatterns = [
-    path('create-payment/', CreatePaymentView.as_view(), name='create-payment'),
-    path('execute-payment/', ExecutePaymentView.as_view(), name='execute-payment'),
-    path('cancel-payment/', CancelPaymentView.as_view(), name='cancel-payment'),
+    path('create-payment/', PaymentCreateView.as_view(), name='create-payment'),
+    path('paypalexecute-payment/', PayPalExecuteView.as_view(), name='execute-payment'),
+    path('cancel-payment/', PaymentHistoryView.as_view(), name='cancelpayment'),
+    path('cancel-payment/', PayPalCancelView.as_view(), name='cancel-payment'),
 ]
